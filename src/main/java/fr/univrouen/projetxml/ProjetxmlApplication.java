5e7191c67076881b7e61b3d9a2efe599e815d2ba
Lydia <lydia@Desdemone.com>
2022-04-13 11:17:25 +0200

    initial commit

---
+++ src/main/java/fr/univrouen/projetxml/ProjetxmlApplication.java
@@ -0,0 +1,13 @@
+package fr.univrouen.projetxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetxmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univrouen/projetxml/ProjetxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.projetxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
